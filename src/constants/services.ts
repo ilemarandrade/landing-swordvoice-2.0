859a56811a50da143903f26dabb1b4b5dbad1862
ilemarandrade <ilemarandrade@gmail.com>
2024-01-08 08:19:53 -0400

    feat: add services data

---
+++ src/constants/services.ts
@@ -0,0 +1,49 @@
+import FolderIcon from "@/assets/icons/FolderIcon";
+import LinkedinIcon from "@/assets/icons/LinkedinIcon";
+import LogoSwordvoice from "@/assets/icons/LogoSwordvoice";
+import UserBadgeIcon from "@/assets/icons/UserBadgeIcon";
+import UserCircleIcon from "@/assets/icons/UserCircleIcon";
+import UserInComputer from "@/assets/icons/UserInComputerIcon";
+import UsersIcon from "@/assets/icons/UsersIcon";
+
+const services = [
+  {
+    title: "Guía",
+    description: "Te enlazamos con muchos reclutadores de todo el mundo",
+    Icon: UserCircleIcon,
+  },
+  {
+    title: "Te asignamos un Mentor",
+    description: "Te guiará por el path de aprendizaje que hayas elegido",
+    Icon: UsersIcon,
+  },
+  {
+    title: "Entrenamiento",
+    description: "Te entrenamos para las entrevistas (HR y técnicas)",
+    Icon: UserBadgeIcon,
+  },
+  {
+    title: "Portafolio",
+    description: "Te orientamos en la elaboración de tu portafolio online",
+    Icon: FolderIcon,
+  },
+  {
+    title: "Ayuda",
+    description: "Te ayudamos a tener tu primer trabajo en el path elegido.",
+    Icon: UserInComputer,
+  },
+  {
+    title: "LinkeIn",
+    description:
+      "Te ayudamos con la creación de un perfil de linkedIn atractivo y competitivo",
+    Icon: LinkedinIcon,
+  },
+  {
+    title: "Comunidad",
+    description:
+      "Además serás miembro de nuestra comunidad online en SwordVoice.com",
+    Icon: LogoSwordvoice,
+  },
+];
+
+export default services;
