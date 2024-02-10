859ca15e257cb764961b64a84c9f8e4a5fbf3ab4
chellysA <chellys.castillo@gmail.com>
2024-02-10 09:59:18 -0400

    feat: update juan's description and photo

--- src/constants/swordvoices.tsx
+++ src/constants/swordvoices.tsx
@@ -1,44 +1,43 @@
 const swordvoices = [
-    {
-        photo: "/swordvoices/jainer.jpeg",
-        name: "Jainer Munoz",
-        description:
-          "CEO, Consultor IT y negocios con 10+ años de experiencia, 8 años de desarrollo web moderno y gestión de equipos en entornos multiculturales.",
-        rol: "Full Stack Engineer",
-        linkedin: "https://www.linkedin.com/in/jainer-munoz/",
-        github: "https://github.com/Yireh2016",
-        portfolio: "https://www.swordvoice.com/",
-      },
-      {
-        photo: "/swordvoices/ilemar.jpg",
-        name: "Ilemar Andrade",
-        description:
-          "Amplia experiencia contribuyendo al desarrollo de software. Desarrollador web y móvil, soñador, líder de equipo y mentor.",
-        rol: "Frontend Developer",
-        linkedin: "https://www.linkedin.com/in/ilemar-andrade-0b261818b/",
-        github: "https://github.com/ilemarandrade",
-        portfolio: "https://ilemarandrade.vercel.app/",
-      },
-      {
-        photo: "/swordvoices/juan.jpeg",
-        name: "Juan Contreras",
-        description:
-          "Gran experiencia utilizando Vanilla JS, Bootstrap, React, Dart y Flutter, entre otros Frameworks; con cualidades para el liderazgo, la enseñanza y gestión de equipos y/o proyectos.",
-        rol: "Fullstack Developer",
-        linkedin: "https://www.linkedin.com/in/contrerasjuanc/",
-        github: "https://github.com/contrerju",
-        portfolio: "/",
-      },
-    {
-      photo: "/swordvoices/chellys.jpg",
-      name: "Chellys Castillo",
-      description:
-        `Mentora, eficiente en el desarrollo de interfaces de usuarios, minusiosa y creativa en el diseño "pixel to pixel" y responsivo de apps.`,
-      rol: "Frontend Deveveloper",
-      linkedin: "https://www.linkedin.com/in/chellys-castillo/",
-      github: "https://github.com/chellysA",
-      portfolio: "https://chellys-castillo-portfolio.vercel.app/",
-    },
-  ];
+  {
+    photo: "/swordvoices/jainer.jpeg",
+    name: "Jainer Munoz",
+    description:
+      "CEO, Consultor IT y negocios con 10+ años de experiencia, 8 años de desarrollo web moderno y gestión de equipos en entornos multiculturales.",
+    rol: "Full Stack Engineer",
+    linkedin: "https://www.linkedin.com/in/jainer-munoz/",
+    github: "https://github.com/Yireh2016",
+    portfolio: "https://www.swordvoice.com/",
+  },
+  {
+    photo: "/swordvoices/ilemar.jpg",
+    name: "Ilemar Andrade",
+    description:
+      "Amplia experiencia contribuyendo al desarrollo de software. Desarrollador web y móvil, soñador, líder de equipo y mentor.",
+    rol: "Frontend Developer",
+    linkedin: "https://www.linkedin.com/in/ilemar-andrade-0b261818b/",
+    github: "https://github.com/ilemarandrade",
+    portfolio: "https://ilemarandrade.vercel.app/",
+  },
+  {
+    photo: "/swordvoices/juan.jpeg",
+    name: "Juan Contreras",
+    description:
+      "Profesional de IT con mas de 20 años de experiencia en Gestion de Equipos y/o Proyectos; 4 años en Desarrollo Movil y Web. Interés en el desarrollo de las capacidades de otros (Mentorias).",
+    rol: "Fullstack Developer",
+    linkedin: "https://www.linkedin.com/in/contrerasjuanc/",
+    github: "https://github.com/contrerju",
+    portfolio: "/",
+  },
+  {
+    photo: "/swordvoices/chellys.jpg",
+    name: "Chellys Castillo",
+    description: `Mentora, eficiente en el desarrollo de interfaces de usuarios, minusiosa y creativa en el diseño "pixel to pixel" y responsivo de apps.`,
+    rol: "Frontend Deveveloper",
+    linkedin: "https://www.linkedin.com/in/chellys-castillo/",
+    github: "https://github.com/chellysA",
+    portfolio: "https://chellys-castillo-portfolio.vercel.app/",
+  },
+];
 
-  export default swordvoices
+export default swordvoices;
